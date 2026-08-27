249b1b405a75f325bf389fa279f42a333ac386a1
gamepunk <17798615+gamepunk@users.noreply.github.com>
2026-08-28 06:38:37 +0800

    Use #{version} interpolation in cask URL

--- Casks/magic-mirror.rb
+++ Casks/magic-mirror.rb
@@ -2,7 +2,7 @@ cask "magic-mirror" do
   version "0.1.0"
   sha256 "45876d11ddc69f956fa2f3baad692a676bc94ceb76cd9de6e0ea31adacb810e1"
 
-  url "https://gamepunk.github.io/magic-mirror/downloads/MagicMirror-0.1.0.dmg"
+  url "https://gamepunk.github.io/magic-mirror/downloads/MagicMirror-#{version}.dmg"
   name "Magic Mirror"
   desc "把任意应用窗口或屏幕区域实时镜像到悬浮窗"
   homepage "https://gamepunk.github.io/magic-mirror/"
